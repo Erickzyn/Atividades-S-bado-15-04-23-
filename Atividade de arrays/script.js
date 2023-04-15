99d20371f0ee17c9ae3f9346febefa9baafb470a
Érick Michael <108885379+Erickzyn@users.noreply.github.com>
2023-04-15 11:36:37 -0300

    Add files via upload

---
+++ Atividade de arrays/script.js
@@ -0,0 +1,23 @@
+// Escreva um script que remova o ultimo elemento do Array
+
+const cars = ["Supra", "GTR", "RX7", "X3", "Ferrari", "Mobi"];
+
+cars.pop();
+
+console.log(cars);
+
+// Escreva um script que adiciona um novo elemento no início do Array
+
+const pc = ["Dell", "Lenovo", "Positivo", "Alienware"];
+
+pc.unshift("Samsung");
+
+console.log(pc);
+
+// Escreva um script que adiciona um elemento no final do Array
+
+const Tênis = ["Adidas", "Nike", "Olympikus"]
+
+Tênis.push("Fila");
+
+console.log(Tênis);
